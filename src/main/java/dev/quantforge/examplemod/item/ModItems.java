6decda5be02801b900365bd2b5856c1b2a277bc2
Cramer-Lawrence <cramer.lawrence@msn.com>
2025-05-10 10:36:49 -0600

    Read documentation, don't watch youtube.

--- src/main/java/dev/quantforge/examplemod/item/ModItems.java
+++ src/main/java/dev/quantforge/examplemod/item/ModItems.java
@@ -9,7 +9,10 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ExampleMod.MOD_ID);
 
-    public static final DeferredItem<Item> BISMUTH = ITEMS.registerItem("bismuth", Item::new, new Item.Properties());
+    public static final DeferredItem<Item> BISMUTH = ITEMS.registerSimpleItem(
+            "bismuth",
+            new Item.Properties()
+    );
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
